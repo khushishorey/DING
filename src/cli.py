f040caf4df2425bdbb453c0f113e9889346fe2ca
icyfire8 <ankit2006pal@gmail.com>
2025-12-30 02:42:18 +0530

    Add ding dong message to CLI output

--- src/cli.py
+++ src/cli.py
@@ -33,9 +33,9 @@ def parse_args():
 
 
 def main():
-    args = parse_args()
-    args.func(args)
-
+    print("Ding Dong, who's there")
+    print("The IRS")
+    print("Oh Shit")
 
 if __name__ == "__main__":
     main()
